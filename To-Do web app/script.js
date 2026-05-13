6e9b4358e42f7a7d7a1a3ef15a3d167cf30df60a
Mohd Shahbaz Khan <mohdshahbazkhan0707@gmail.com>
2026-05-14 00:14:17 +0530

    Add files via upload

---
+++ To-Do web app/script.js
@@ -0,0 +1,30 @@
+function addTask() {
+    const input = document.getElementById("taskInput");
+    const taskText = input.value.trim();
+
+    if (taskText === "") return;
+
+    const li = document.createElement("li");
+
+    const span = document.createElement("span");
+    span.textContent = taskText;
+
+    span.onclick = function () {
+        li.classList.toggle("completed");
+    };
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.classList.add("delete-btn");
+
+    deleteBtn.onclick = function () {
+        li.remove();
+    };
+
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
